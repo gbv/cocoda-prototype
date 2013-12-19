a4ef32f91d90c99004f6c8e98fee10857157b830
Jakob Voss <voss@gbv.de>
2013-12-19 15:14:50 +0100

    improved JSON client

--- lib/Cocoda/Role/JSONClient.pm
+++ lib/Cocoda/Role/JSONClient.pm
@@ -1,20 +1,32 @@
 package Cocoda::Role::JSONClient;
+#ABSTRACT: HTTP client to query a JSON API
 use v5.14;
 use Moo::Role;
-
 use Furl;
 use JSON;
+
+has agent => (
+    is => 'ro',
+    default => sub { 'Mozilla/5.0' }
+);
 
 has furl => (
     is => 'ro',
     lazy => 1,
-    default => sub { Furl->new( timeout => 10, agent => 'Mozilla/5.0' ) }
+    builder => sub { Furl->new( timeout => 10, agent => $_[0]->agent ) }
 );
 
-sub jsonclient {
-    my ($self, $url) = @_;
+has base => (
+    is => 'ro',
+    required => 1
+);
+
+sub get {
+    my ($self, $path, %query) = @_;
 
     # TODO: better catch errors
+
+    my $url = $self->base . $path; # TODO: add %query
     my $res = $self->furl->get($url);
     return unless $res->is_success;
     my $data = JSON->new->utf8(0)->decode($res->content);
--- lib/Cocoda/Terminology/RVK.pm
+++ lib/Cocoda/Terminology/RVK.pm
@@ -11,6 +11,10 @@ has '+title' => (
     default => sub { 'Regensburger Verbundklassifikation' }
 );
 
+has '+base' => (
+    default => sub { 'http://rvk.uni-regensburg.de/api/json' }
+);
+
 sub debug { # TODO: move to logging module
     use Data::Dumper;
     say STDERR ref($_[0]) ? Dumper($_[0]) : $_[0]; 
@@ -20,16 +24,14 @@ sub concept {
     my ($self, $notation, %options) = @_;
 
     $notation =~ s/[ _]/+/g;
-    my $url = "http://rvk.uni-regensburg.de/api/json/node/$notation";
-    my $data = $self->jsonclient($url) or return;
+    my $data = $self->get("/node/$notation") or return;
 
     # debug($data);
 
     my $concept = rvk_to_cocoda( $data->{node} );
 
     if ($options{ancestors}) {
-        my $url = "http://rvk.uni-regensburg.de/api/json/ancestors/$notation";
-        if (my $data = $self->jsonclient($url)) {
+        if (my $data = $self->get("/ancestors/$notation")) {
             # debug($data);
             my $nodes = $data->{node};
             $nodes = [$nodes] unless ref $nodes eq 'ARRAY';
@@ -40,8 +42,7 @@ sub concept {
     }
 
     if ($options{narrower}) {
-        my $url = "http://rvk.uni-regensburg.de/api/json/children/$notation";
-        if (my $data = $self->jsonclient($url)) {
+        if (my $data = $self->get("/children/$notation")) {
             $concept->{narrower} = [
                 map { rvk_to_cocoda($_) } @{$data->{node}{children}{node}}
             ]
@@ -49,6 +50,22 @@ sub concept {
     }
 
     return $concept;
+}
+
+sub search {
+    my ($self, $query) = @_;
+
+    $query = uri_escape($query);
+    my $data = $self->get("/nodes/$query") or return;
+
+    return [ map { rvk_to_cocoda($_) } @{$data->{node}} ];
+}
+
+sub top {
+    my ($self) = @_;
+
+    my $data = $self->get("/children") or return;
+    return [ map { rvk_to_cocoda($_) } @{$data->{node}{children}{node}} ];
 }
 
 sub rvk_to_cocoda {
@@ -67,24 +84,6 @@ sub rvk_to_cocoda {
         if ($node->{has_children} // 'yes') ne 'yes';
 
     return $concept;
-}
-
-sub search {
-    my ($self, $query) = @_;
-
-    $query = uri_escape($query);
-    my $url = "http://rvk.uni-regensburg.de/api/json/nodes/$query";
-    my $data = $self->jsonclient($url) or return;
-
-    return [ map { rvk_to_cocoda($_) } @{$data->{node}} ];
-}
-
-sub top {
-    my ($self) = @_;
-
-    my $url = "http://rvk.uni-regensburg.de/api/json/children";
-    my $data = $self->jsonclient($url) or return;
-    return [ map { rvk_to_cocoda($_) } @{$data->{node}{children}{node}} ];
 }
 
 1;
