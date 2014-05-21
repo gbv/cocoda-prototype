8e5c079ea137843387f3d2b7730d7a01931724c7
Moritz Horn <moritz.horn@gmail.com>
2014-05-21 15:03:25 +0200

    demo with suggest

---
+++ demo/demo2.js
@@ -0,0 +1,22 @@
+var Demo = angular.module('myApp', ['ngSKOS','ui.bootstrap','ngSuggest']);
+Demo.run(function($rootScope,$http) {
+    $rootScope.sampleConcept = {};
+    $http.get('data/concept-1.json').success(function(data){
+        $rootScope.sampleConcept = data;
+    });
+});
+Demo.config(function($locationProvider) {
+          $locationProvider.html5Mode(true);
+        }).controller('MainCtrl', function ($scope, $location) { });
+        
+function myController($rootScope){
+    $rootScope.example = {api: "http://ws.gbv.de/suggest/gnd/index.php?count=10&type=&searchterm="}
+    $rootScope.example.input = "";
+    $rootScope.example.onSelect = selectFunction();
+    
+    function selectFunction() {
+        return function (item) {
+            $rootScope.example.item = item;
+        }
+    }
+}
