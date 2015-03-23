ec46d83e6c0839698cc87fa35106fb70255e7713
hornmo <moritz.horn@gmail.com>
2015-03-23 20:18:59 +0100

    ddc concepts are now loaded dynamically

--- cocoda.js
+++ cocoda.js
@@ -226,7 +226,48 @@ function (OpenSearchSuggestions, SkosConceptSource, SkosConceptListSource) {
     };
     
     this.ddc = {
-        name: 'DDC'
+        name: 'DDC',
+        getConcept: new SkosConceptSource({
+            url:"http://esx-151.gbv.de/?view=notation&key={notation}",
+            transform: function(item){
+                var c = item[0].value;
+                var concept = {
+                    notation: c.notation,
+                    prefLabel: c.prefLabel,
+                    broader: c.broader
+                };
+                return concept;
+            },
+            jsonp: true
+        }),
+
+        getNarrower: new SkosConceptSource({
+            url:"http://esx-151.gbv.de/?view=broader&key={notation}",
+            transform: function(item){
+                var concept = {
+                    narrower: []
+                }
+                angular.forEach(item, function(nterm){
+                    concept.narrower.push({ notation: nterm.value.notation, prefLabel: nterm.value.prefLabel });
+                });  
+                return concept;
+            },
+            jsonp: true
+        }),
+        suggest: new OpenSearchSuggestions({
+            url: "http://esx-151.gbv.de/?view=prefLabel&key=",
+            transform: function(response) {
+                return {
+                    values: response.map(function(s) {
+                        return {
+                            label: s.key,
+                            notation: s.value.notation[0]
+                        };
+                    }),
+                };
+            },
+            jsonp: true
+        }),
     };
     
     this.wikidata = {
@@ -255,9 +296,14 @@ cocoda.controller('myController',[
 
     $scope.gndSubjectConcept = cocodaSchemes.gnd.getConcept;
     $scope.rvkSubjectConcept = cocodaSchemes.rvk.getConcept;
+    $scope.ddcSubjectConcept = cocodaSchemes.ddc.getConcept;
 
     $scope.rvkNarrowerConcepts = cocodaSchemes.rvk.getNarrower;
     $scope.rvkBroaderConcepts = cocodaSchemes.rvk.getBroader;
+    
+    $scope.ddcNarrowerConcepts = cocodaSchemes.ddc.getNarrower;
+    $scope.ddcBroaderConcepts = cocodaSchemes.ddc.getBroader;
+    
 
     // NG-SUGGEST & NAVBAR FUNCTIONALITY
     $scope.showTargetSearch = false;
@@ -593,6 +639,39 @@ cocoda.controller('myController',[
                 notation: [ item.notation ],
                 uri: item.uri,
                 prefLabel: item.prefLabel ? item.prefLabel : "",
+                broader: []
+            };
+            // update concept
+            $scope.ddcSubjectConcept.updateConcept($scope.originConcept).then(function(){
+                $scope.tbConcept = angular.copy($scope.originConcept.broader[0]);  
+            }).then(function(){
+                console.log($scope.tbConcept);
+                $scope.ddcSubjectConcept.updateConcept($scope.tbConcept).then(function(){
+                    $scope.originConcept.broader[0] = angular.copy($scope.tbConcept);
+                });
+            }).then(function(){
+                $scope.tnConcept = angular.copy($scope.originConcept);
+                console.log($scope.originConcept);
+            }).then(function(){
+                $scope.ddcNarrowerConcepts.updateConcept($scope.tnConcept).then(function(){
+                    $scope.originConcept.narrower = angular.copy($scope.tnConcept.narrower);
+                });
+            })
+            $scope.clickOriginConcept = function(concept) {
+                
+                $scope.ddcSubjectConcept.updateConcept($scope.originConcept = concept ).then(function(){
+                    $scope.tbConcept = angular.copy($scope.originConcept.broader[0]);
+                }).then(function(){
+                    $scope.ddcSubjectConcept.updateConcept($scope.tbConcept).then(function(){
+                        $scope.originConcept.broader[0] = angular.copy($scope.tbConcept);
+                    });
+                }).then(function(){
+                    $scope.tnConcept = angular.copy($scope.originConcept);
+                }).then(function(){
+                    $scope.ddcNarrowerConcepts.updateConcept($scope.tnConcept).then(function(){
+                        $scope.originConcept.narrower = angular.copy($scope.tnConcept.narrower);
+                    });
+                })
             };
         }
     };
@@ -670,6 +749,45 @@ cocoda.controller('myController',[
 
                     }
                 });
+            };
+        }else if($scope.activeView.target == 'DDC'){
+            
+            $scope.targetConcept = {
+                notation: [ item.notation ],
+                uri: item.uri,
+                prefLabel: item.prefLabel ? item.prefLabel : "",
+                broader: []
+            };
+            // update concept
+            $scope.ddcSubjectConcept.updateConcept($scope.targetConcept).then(function(){
+                $scope.tbConcept = angular.copy($scope.targetConcept.broader[0]);  
+            }).then(function(){
+                $scope.ddcSubjectConcept.updateConcept($scope.tbConcept).then(function(){
+                    $scope.targetConcept.broader[0] = angular.copy($scope.tbConcept);
+                });
+            }).then(function(){
+                $scope.tnConcept = angular.copy($scope.targetConcept);
+            }).then(function(){
+                $scope.ddcNarrowerConcepts.updateConcept($scope.tnConcept).then(function(){
+                    $scope.targetConcept.narrower = angular.copy($scope.tnConcept.narrower);
+                });
+            })
+            $scope.clickTargetConcept = function(concept) {
+                
+                $scope.ddcSubjectConcept.updateConcept($scope.targetConcept = concept ).then(function(){
+                    $scope.tbConcept = angular.copy($scope.targetConcept.broader[0]);  
+                    console.log($scope.tbConcept);
+                }).then(function(){
+                    $scope.ddcSubjectConcept.updateConcept($scope.tbConcept).then(function(){
+                        $scope.targetConcept.broader[0] = angular.copy($scope.tbConcept);
+                    });
+                }).then(function(){
+                    $scope.tnConcept = angular.copy($scope.targetConcept);
+                }).then(function(){
+                    $scope.ddcNarrowerConcepts.updateConcept($scope.tnConcept).then(function(){
+                        $scope.targetConcept.narrower = angular.copy($scope.tnConcept.narrower);
+                    });
+                })
             };
         }
     };
